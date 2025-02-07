0dd7ac192bf8520adf5fe64e1406083645522e9a
camilaponcebc <camilaponcebc@gmail.com>
2025-02-07 15:55:45 -0600

    Add files via upload

---
+++ cami.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import math
+dir(math)
+
+"""
+F = float(input('ingrese un valor para la temperatura en grados Farenheit: '))
+
+C = (F-32)*5/9
+
+print(f'Temperatura en grados celsuis es {C}')
+
+x=5
+a=math.sinh(5)
+print('El seno hiperbolico de 5 es', a)
+
+b=(math.e**x-math.e**-x)/2
+print('La ecuación a es igual a: ', b)
+
+c = ((math.exp(x)-math.exp(-x))/2)
+print('La ecuación a es igual b: ', c)
+"""
+z1=8+4j
+z2=2j
+w=z1+z2
+w.conjugate()
+#w.imag
+#w.real
+
+
