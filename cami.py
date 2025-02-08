52a5be3cfd2ca292460909803b1bfafc13ca35f0
camilaponcebc <camilaponcebc@gmail.com>
2025-02-07 21:37:57 -0700

    ej1 y 2.py

--- cami.py
+++ cami.py
@@ -6,14 +6,15 @@ This is a temporary script file.
 """
 import math
 dir(math)
+#EJERCICIO 1 DE CLASE
 
-"""
 F = float(input('ingrese un valor para la temperatura en grados Farenheit: '))
 
 C = (F-32)*5/9
 
 print(f'Temperatura en grados celsuis es {C}')
 
+#EJERCICIO 2 DE CLASE
 x=5
 a=math.sinh(5)
 print('El seno hiperbolico de 5 es', a)
@@ -23,7 +24,7 @@ print('La ecuación a es igual a: ', b)
 
 c = ((math.exp(x)-math.exp(-x))/2)
 print('La ecuación a es igual b: ', c)
-"""
+
 z1=8+4j
 z2=2j
 w=z1+z2
